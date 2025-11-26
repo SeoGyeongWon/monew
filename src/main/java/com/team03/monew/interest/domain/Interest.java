b0eb6a1d3be4e24a96e25efbe30fb85877b7db62
jihoon-68 <pkrwlgns64@gmail.com>
2025-11-26 14:01:42 +0900

    [박지훈] feat : Interest Entity 생성

--- src/main/java/com/team03/monew/interest/domain/Interest.java
+++ src/main/java/com/team03/monew/interest/domain/Interest.java
@@ -1,4 +1,56 @@
 package com.team03.monew.interest.domain;
 
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
 public class Interest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 100)
+    private String name;
+
+
+    @Column(columnDefinition = "varchar(100)[]")
+    private List<String> keywords;
+
+    @CreatedDate
+    private Instant createdAt;
+
+    @LastModifiedDate
+    private Instant updatedAt;
+
+    public Interest() {}
+
+    @Builder
+    public Interest(String name, List<String> keywords) {
+        this.name = name;
+        this.keywords = keywords;
+    }
+
+    void keywordAdd(String keyword) {
+        this.keywords.add(keyword);
+    }
+
+    void keywordRemove(String keyword) {
+        this.keywords.remove(keyword);
+    }
+
+    //관심사 유사도 검사 메소드
+    boolean nameEquals(String name) {
+        int nameLength = this.name.length();
+        int targetLength = name.length();
+        return name.contains(this.name) && (targetLength * 0.8) <= nameLength;
+    }
 }
