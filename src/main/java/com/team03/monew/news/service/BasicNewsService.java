29263afec50aad2b7977a5de3b72b4ee1a356e7d
SeoGyeongWon <skwknh00@naver.com>
2025-12-01 01:19:11 +0900

    [서경원] Chore: 뉴스 등록시 사용하지 않는 import 제거

--- src/main/java/com/team03/monew/news/service/BasicNewsService.java
+++ src/main/java/com/team03/monew/news/service/BasicNewsService.java
@@ -2,11 +2,9 @@ package com.team03.monew.news.service;
 
 import com.team03.monew.news.domain.News;
 import com.team03.monew.news.dto.NewsCreateRequest;
-import com.team03.monew.news.dto.NewsDto;
 import com.team03.monew.news.dto.NewsResponseDto;
 import com.team03.monew.news.exception.CustomException.SameResourceLink;
 import com.team03.monew.news.repository.NewsRepository;
-import java.time.LocalDateTime;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
