a86287cc14f243c9698e3e64665bccf526cfd45f
SeoGyeongWon <skwknh00@naver.com>
2025-12-01 01:17:14 +0900

    [서경원] Fix: 뉴스 등록간 입력받는 값 타입 변경

--- src/main/java/com/team03/monew/news/dto/NewsCreateRequest.java
+++ src/main/java/com/team03/monew/news/dto/NewsCreateRequest.java
@@ -1,12 +1,13 @@
 package com.team03.monew.news.dto;
 
 import com.team03.monew.news.domain.NewsSourceType;
+import java.time.LocalDateTime;
 
 public record NewsCreateRequest(
     NewsSourceType source,
     String resourceLink,
     String title,
-    String postDate,
+    LocalDateTime postDate,
     String overView
 ) {
 
