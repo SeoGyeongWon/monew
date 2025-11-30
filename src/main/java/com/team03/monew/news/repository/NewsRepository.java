6ad1c537bea827621863d3daedc9a64d1e090daf
SeoGyeongWon <skwknh00@naver.com>
2025-12-01 01:17:49 +0900

    [서경원] Feat: 뉴스 레포토리 뉴스 링크를 이용한 값찾기 추가

--- src/main/java/com/team03/monew/news/repository/NewsRepository.java
+++ src/main/java/com/team03/monew/news/repository/NewsRepository.java
@@ -1,11 +1,12 @@
 package com.team03.monew.news.repository;
 
 import com.team03.monew.news.domain.News;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface NewsRepository extends JpaRepository<News, UUID>{
-
+  Optional<News> findByResourceLink(String resourceLink);
 }
